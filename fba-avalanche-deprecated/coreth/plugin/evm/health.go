8edc3fcc07bbf8ac442e46a6d8fd45de1fdb3340
Sean Rowan <sean@flare.network>
2020-11-12 19:54:03 -0500

    Separate network config and network launch scripts

--- fba-avalanche-deprecated/coreth/plugin/evm/health.go
+++
@@ -1,9 +0,0 @@
-package evm
-
-// Health returns nil if this chain is healthy.
-// Also returns details, which should be one of:
-// string, []byte, map[string]string
-func (vm *VM) Health() (interface{}, error) {
-	// TODO perform actual health check
-	return nil, nil
-}
--- launch.sh
+++ launch.sh
@@ -1,33 +1,14 @@
 #!/bin/bash
 
 NODE_VERSION=@v1.0.4
-CORETH_VERSION=@v0.3.12
 
-rm -rf logs
-mkdir logs
 LOG_DIR=$(pwd)/logs
-CONFIG_DIR=$(pwd)/config
 PKG_DIR=$GOPATH/pkg/mod/github.com/ava-labs
 NODE_DIR=$PKG_DIR/avalanchego$NODE_VERSION
-CORETH_DIR=$PKG_DIR/coreth$CORETH_VERSION
-
-rm -rf $NODE_DIR
-mkdir -p $PKG_DIR
-cp -r fba-avalanche/avalanchego $NODE_DIR
-
-rm -rf $CORETH_DIR
-cp -r fba-avalanche/coreth $CORETH_DIR
-
 cd $NODE_DIR
-rm -rf $NODE_DIR/db/
-
-printf "\x1b[34mFlare Network 5-Node Local Deployment\x1b[0m\n\n"
-printf "Building Flare Core...\n"
-./scripts/build.sh
 
 # NODE 1
 printf "\nLaunching Node 1 at 127.0.0.1:9650\n"
-mkdir -p $LOG_DIR/node00
 nohup ./build/avalanchego --public-ip=127.0.0.1 --snow-sample-size=2 --snow-quorum-size=2 --http-port=9650 --staking-port=9651 --db-dir=db/node00 --staking-enabled=true --network-id=flare --bootstrap-ips= --bootstrap-ids= --staking-tls-cert-file=$(pwd)/keys/node00/staker.crt --staking-tls-key-file=$(pwd)/keys/node00/staker.key --log-level=debug --unl-validators=\
 $(cat $(pwd)/keys/node00/nodeID.txt),\
 $(cat $(pwd)/keys/node01/nodeID.txt),\
@@ -39,7 +20,6 @@ sleep 5
 
 # NODE 2
 printf "Launching Node 2 at 127.0.0.1:9652\n"
-mkdir -p $LOG_DIR/node01
 nohup ./build/avalanchego --public-ip=127.0.0.1 --snow-sample-size=2 --snow-quorum-size=2 --http-port=9652 --staking-port=9653 --db-dir=db/node01 --staking-enabled=true --network-id=flare --bootstrap-ips=127.0.0.1:9651 --bootstrap-ids=$(cat $(pwd)/keys/node00/nodeID.txt) --staking-tls-cert-file=$(pwd)/keys/node01/staker.crt --staking-tls-key-file=$(pwd)/keys/node01/staker.key --log-level=debug --unl-validators=\
 $(cat $(pwd)/keys/node00/nodeID.txt),\
 $(cat $(pwd)/keys/node01/nodeID.txt),\
@@ -51,7 +31,6 @@ sleep 5
 
 # NODE 3
 printf "Launching Node 3 at 127.0.0.1:9654\n"
-mkdir -p $LOG_DIR/node02
 nohup ./build/avalanchego --public-ip=127.0.0.1 --snow-sample-size=2 --snow-quorum-size=2 --http-port=9654 --staking-port=9655 --db-dir=db/node02 --staking-enabled=true --network-id=flare --bootstrap-ips=127.0.0.1:9651 --bootstrap-ids=$(cat $(pwd)/keys/node00/nodeID.txt) --staking-tls-cert-file=$(pwd)/keys/node02/staker.crt --staking-tls-key-file=$(pwd)/keys/node02/staker.key --log-level=debug --unl-validators=\
 $(cat $(pwd)/keys/node00/nodeID.txt),\
 $(cat $(pwd)/keys/node01/nodeID.txt),\
@@ -63,7 +42,6 @@ sleep 5
 
 # NODE 4
 printf "Launching Node 4 at 127.0.0.1:9656\n"
-mkdir -p $LOG_DIR/node03
 nohup ./build/avalanchego --public-ip=127.0.0.1 --snow-sample-size=2 --snow-quorum-size=2 --http-port=9656 --staking-port=9657 --db-dir=db/node03 --staking-enabled=true --network-id=flare --bootstrap-ips=127.0.0.1:9651 --bootstrap-ids=$(cat $(pwd)/keys/node00/nodeID.txt) --staking-tls-cert-file=$(pwd)/keys/node03/staker.crt --staking-tls-key-file=$(pwd)/keys/node03/staker.key --log-level=debug --unl-validators=\
 $(cat $(pwd)/keys/node00/nodeID.txt),\
 $(cat $(pwd)/keys/node01/nodeID.txt),\
@@ -75,7 +53,6 @@ sleep 5
 
 # NODE 5
 printf "Launching Node 5 at 127.0.0.1:9658\n"
-mkdir -p $LOG_DIR/node04
 nohup ./build/avalanchego --public-ip=127.0.0.1 --snow-sample-size=2 --snow-quorum-size=2 --http-port=9658 --staking-port=9659 --db-dir=db/node04 --staking-enabled=true --network-id=flare --bootstrap-ips=127.0.0.1:9651 --bootstrap-ids=$(cat $(pwd)/keys/node00/nodeID.txt) --staking-tls-cert-file=$(pwd)/keys/node04/staker.crt --staking-tls-key-file=$(pwd)/keys/node04/staker.key --log-level=debug --unl-validators=\
 $(cat $(pwd)/keys/node01/nodeID.txt),\
 $(cat $(pwd)/keys/node02/nodeID.txt),\
@@ -135,11 +112,8 @@ curl -sX POST --data '{
     }
 }' -H 'content-type:application/json;' 127.0.0.1:9658/ext/P | jq '.result'
 
-cd - &>/dev/null
-printf "\nNetwork launched, deploying state-connector system..."
-node deploy.js
-printf "\nNode endpoints configured, stopping network..."
-sleep 5
+printf "\n\n"
+read -p "Press enter to stop background node processes"
 kill $NODE_00_PID
 kill $NODE_01_PID
 kill $NODE_02_PID
