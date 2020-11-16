848d0ad2eca1944c788b1ff6fbcce6ac2b5c2e6c
Sean Rowan <sean@flare.network>
2020-11-16 08:21:25 -0500

    Update README.md, remove deprecated files

--- fba-avalanche-deprecated/avalanchego/snow/engine/common/queue/parser.go
+++
@@ -1,9 +0,0 @@
-// (c) 2019-2020, Ava Labs, Inc. All rights reserved.
-// See the file LICENSE for licensing terms.
-
-package queue
-
-// Parser ...
-type Parser interface {
-	Parse([]byte) (Job, error)
-}
