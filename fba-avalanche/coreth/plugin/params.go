6759fbf7075524bcc984ced76183b8391d86acb8
Sean Rowan <sean@flare.network>
2021-04-11 20:18:11 +0000

    Update fba-avalanche/coreth/plugin/params.go

--- fba-avalanche/coreth/plugin/params.go
+++ fba-avalanche/coreth/plugin/params.go
@@ -42,7 +42,6 @@ func init() {
 	cliConfig.TxPoolAPIEnabled = true
 	cliConfig.NetAPIEnabled = true
 	cliConfig.Web3APIEnabled = true
-	cliConfig.DebugAPIEnabled = true
 	cliConfig.RPCGasCap = 2500000000 // 25000000 x 100
 	cliConfig.RPCTxFeeCap = 100      // 100 AVAX
 
