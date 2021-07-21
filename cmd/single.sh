f44c91819065390b96cf8cb08d36eb78d33e96d3
Sean Rowan <sean@flare.network>
2021-07-21 14:36:32 -0400

    scdev genesis, compile.sh updated

---
+++ cmd/single.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+if [[ $(pwd) =~ " " ]]; then echo "Working directory path contains a folder with a space in its name, please remove all spaces" && exit; fi
+if [ -z ${GOPATH+x} ]; then echo "GOPATH is not set, visit https://github.com/golang/go/wiki/SettingGOPATH" && exit; fi
+printf "\x1b[34mFlare Network 4-Node Local Deployment\x1b[0m\n\n"
+
+LAUNCH_DIR=$(pwd)
+export FBA_VALs=$LAUNCH_DIR/conf/local/fba_validators.json
+AVALANCHE_DIR=$GOPATH/src/github.com/ava-labs/avalanchego
+cd $AVALANCHE_DIR
+if ! echo $1 | grep -e "--existing" -q
+then
+	rm -rf $LAUNCH_DIR/logs/local
+	mkdir -p $LAUNCH_DIR/logs/local
+	rm -rf $LAUNCH_DIR/db/local
+	mkdir -p $LAUNCH_DIR/db/local
+	mkdir -p $LAUNCH_DIR/logs/local/node1
+	mkdir -p $LAUNCH_DIR/logs/local/node2
+	mkdir -p $LAUNCH_DIR/logs/local/node3
+	mkdir -p $LAUNCH_DIR/logs/local/node4
+	mkdir -p $LAUNCH_DIR/logs/local/node5
+	mkdir -p $LAUNCH_DIR/db/local/node1
+	mkdir -p $LAUNCH_DIR/db/local/node2
+	mkdir -p $LAUNCH_DIR/db/local/node3
+	mkdir -p $LAUNCH_DIR/db/local/node4
+	mkdir -p $LAUNCH_DIR/db/local/node5
+fi
+
+# NODE 1
+printf "Launching Node 1 at 127.0.0.1:9650\n"
+./build/avalanchego \
+--public-ip=127.0.0.1 \
+--snow-sample-size=1 \
+--snow-quorum-size=1 \
+--http-port=9660 \
+--staking-port=9661 \
+--log-dir=$LAUNCH_DIR/logs/local/node1 \
+--db-dir=$LAUNCH_DIR/db/local/node1 \
+--bootstrap-ips= \
+--bootstrap-ids= \
+--staking-enabled=false \
+--staking-tls-cert-file=$LAUNCH_DIR/conf/local/node1/node.crt \
+--staking-tls-key-file=$LAUNCH_DIR/conf/local/node1/node.key \
+--log-level=info \
+--db-type=leveldb
+
--- compile.sh
+++ compile.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+if [[ $(pwd) =~ " " ]]; then echo "Working directory path contains a folder with a space in its name, please remove all spaces" && exit; fi
+if [ -z ${GOPATH+x} ]; then echo "GOPATH is not set, visit https://github.com/golang/go/wiki/SettingGOPATH" && exit; fi
 WORKING_DIR=$(pwd)
 
 sudo rm -rf $GOPATH/src/github.com/ava-labs
@@ -9,10 +11,16 @@ git checkout ac32de45ffd6769007f250f123a5d5dae8230456
 
 echo "Applying Flare-specific changes to AvalancheGo..."
 
+GENESIS_FILE=genesis_coston.go
+if [ $# -ne 0 ]
+  then
+    GENESIS_FILE=$1
+fi
+
 # Apply changes to avalanchego
+cp $WORKING_DIR/src/genesis/$GENESIS_FILE ./genesis/genesis_coston.go
 cp $WORKING_DIR/src/avalanchego/flags.go ./config/flags.go
 cp $WORKING_DIR/src/avalanchego/beacons.go ./genesis/beacons.go
-cp $WORKING_DIR/src/avalanchego/genesis_coston.go ./genesis/genesis_coston.go
 cp $WORKING_DIR/src/avalanchego/genesis_fuji.go ./genesis/genesis_fuji.go
 cp $WORKING_DIR/src/avalanchego/unparsed_config.go ./genesis/unparsed_config.go
 cp $WORKING_DIR/src/avalanchego/set.go ./snow/validators/set.go
