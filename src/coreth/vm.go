9be14f5b4b2e740d36996f0941c05b5486bf8b27
Sean Rowan <sean@flare.network>
2021-09-07 19:21:46 +0000

    Update src/coreth/vm.go

--- src/coreth/vm.go
+++ src/coreth/vm.go
@@ -296,7 +296,7 @@ func (vm *VM) Initialize(
 		vm.config.Web3APIEnabled = true
 		vm.config.DebugAPIEnabled = true
 		vm.config.TxPoolAPIEnabled = true
-		vm.config.MaxBlocksPerRequest = 100
+		vm.config.MaxBlocksPerRequest = 0
 	}
 
 	if b, err := json.Marshal(vm.config); err == nil {
